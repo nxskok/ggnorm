719048b71f056fb1ee3f2d1294e4d05d3c159e46
nxskok <butler@utsc.utoronto.ca>
2016-09-26 21:41:02 -0400

    rename function ggnormplot to avoid conflict with package name

--- R/code.R
+++ R/code.R
@@ -1,13 +1,23 @@
-ggnorm=function(z) {
+#' Draw normal quantile plot with line
+#'
+#' @param z A vector
+#' @return Normal quantile plot with line
+#' @examples
+#' set.seed(1)
+#' ggnormplot(rnorm(100))
+#' ggnormplot(rchisq(100,3))
+#' @export
+ggnormplot=function(z) {
   z=z[!is.na(z)]
   y=quantile(z, c(0.25, 0.75))
   x=qnorm(c(0.25, 0.75))
   b=(y[2]-y[1])/(x[2]-x[1])
-  a=y[1]-slope*x[1]
+  a=y[1]-b*x[1]
 
   d <- data.frame(z)
 
-  ggplot2::ggplot(d, aes(sample = z)) + stat_qq() +
+  ggplot(d, aes(sample = z)) + stat_qq() +
      geom_abline(slope = b, intercept = a)
 
 }
+
---
+++ R/ggnorm.R
@@ -0,0 +1,10 @@
+#' ggnorm: A package for drawing normal quantile plots with lines on ggplot
+#' @docType package
+#' @name ggnorm
+NULL
+
+#' @importFrom stats quantile qnorm
+NULL
+
+#' @importFrom ggplot2 aes geom_abline stat_qq
+NULL
